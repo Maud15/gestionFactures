10ad46125ecc8d068477ba1bf48267135c061f6e
Sego <zoran@LAPTOP-JKURSJPU.home>
2022-12-06 17:08:49 +0100

    Update Bill ok

--- src/main/java/com/m2i/gestionfactures/dao/BillDao.java
+++ src/main/java/com/m2i/gestionfactures/dao/BillDao.java
@@ -1,6 +1,10 @@
 package com.m2i.gestionfactures.dao;
 
 import com.m2i.gestionfactures.model.Bill;
+import com.m2i.gestionfactures.model.Customer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.EntityTransaction;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,7 +12,14 @@ import java.util.Optional;
 public class BillDao implements Dao<Bill>{
     @Override
     public Optional<Bill> get(Long id) {
-        return Optional.empty();
+        Optional<Bill> optBill = Optional.empty();
+        EntityManager em = ConnectionManager.getEntityManager();
+        try {
+            optBill = Optional.of(em.find(Bill.class, id));
+        } catch(EntityNotFoundException e) {
+            e.printStackTrace();
+        }
+        return optBill;
     }
 
     @Override
@@ -18,6 +29,19 @@ public class BillDao implements Dao<Bill>{
 
     @Override
     public void update(Bill bill) {
+
+        EntityManager em = ConnectionManager.getEntityManager();
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+            em.merge(bill);
+            et.commit();
+        } catch(Exception e) {
+            if(et.isActive()) {et.rollback();}
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
 
     }
 
